dd47e08b005ba18d183e55777eab29d8867ed554
Pavel <pvk19941@rambler.ru>
2022-03-28 16:35:39 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    Computer comp = new Computer();
+        comp.i7.start();
+        comp.transfer.start();
+        comp.transfer.shutdown();
+        // Код получает доступ к классам , вложенным в другом классе-Computer.
+    }
+}
